94532d1c1dbbed5f093410508854192dfaf46145
Brance Pearson <brancepearson@gmail.com>
2017-10-25 13:51:28 -0700

    this will clean up JS code and add more comments

--- app.js
+++ app.js
@@ -1,64 +1,73 @@
 'use strict';
 
-//Initial question on guitar.
-var play = prompt('Can I play the guitar?');
-if (play.toLowerCase() === 'yes' || play.toLowerCase() === 'y' ) { 
-    confirm( 'You\'re correct! I do.');
-    console.log( 'do i play guitar?', play);
+// Greeting asking user's name
+const username = prompt( 'Hello and welcome. What is your name?' );
+confirm('Hi ' + username);
+
+//Initial question on if I can play guitar.
+const play = prompt(username + ', can I play the guitar?');
+if (play.toLowerCase() === 'yes' || play.toLowerCase() === 'y' ) {
+    confirm( 'You\'re correct,' + '. I do.');
+    console.log( 'Do i play guitar? Answer:', play);
 } else if ( play.toLowerCase() === 'no' || play.toLowerCase() === 'n' ) {
     alert( 'Actually I do play guitar.' );
-    console.log( 'do i play guitar?', play);
+    console.log( 'Do i play guitar? Answer:', play);
 };
 
-//For question on additional instruments.
-var more = prompt('Do you think I can play any other instruments?');
-if (more.toLowerCase() === 'yes' || more.toLowerCase() === 'y' ) { 
-    var evenMore = prompt( 'Yep. I can play another instrument. How many do you think I can play?'); //technically 3rd question
+//For question on if I play additional instruments.
+const more = prompt('Ok, ' + username + 'Do you think I can play any other instruments?');
+if (more.toLowerCase() === 'yes' || more.toLowerCase() === 'y' ) {
+    console.log( 'Do I play other instruments? Answer:', more);
+    const evenMore = prompt( 'Yep. I can play another instrument. How many do you think I can play?');
+    // 3rd question asking how many inst can I play.
     if ( evenMore === '2' || evenMore === '4' || evenMore === 'two' || evenMore === 'four') {
-        alert( 'Close. I play 3. Mandolin. Guitar. And Bass.')
-        console.log( 'How many instruments?', evenMore)
-    } 
-    else if (evenMore === '3' || evenMore === 'three') {
-        confirm( 'Nailed it! I play 3. Mandolin. Guitar. And Bass.')
-        console.log( 'How many instruments?', evenMore) 
-    } 
-    else {
-        alert( 'It\s cool. Moving on.' )
+        alert( 'OOooo, ' + username + 'close. I play 3 instruments total. Mandolin, Guitar and Bass.');
+        console.log( 'How many instruments can I play? Answer:', evenMore);
     }
-} 
-
-else if ( more.toLowerCase() === 'no' || more.toLowerCase() === 'n' ) {
-    var evenMore = prompt( 'Actually. I can play another instrument. You don\'t know me at all. How many do you think I can play?'); //technically 3rd question
-    if ( evenMore === '2' || evenMore === '4' || evenMore === 'two' || evenMore === 'four') {
-        alert( 'Close. I play 3. Mandolin. Guitar. And Bass.')
-        console.log( 'How many instruments?', evenMore)
-    } 
     else if (evenMore === '3' || evenMore === 'three') {
-        confirm( 'Nailed it! I play 3. Mandolin. Guitar. And Bass.')
-        console.log( 'How many instruments?', evenMore) 
-    } 
+        confirm( 'Nailed it! I play 3. Mandolin, Guitar and Bass.');
+        console.log( 'How many instruments can I play? Answer:', evenMore);
+    }
     else {
-        alert( 'It\s cool. I can tell you\'re bored. Moving on.' )
-        console.log( 'How many instruments?', evenMore)
+        alert( 'It\s cool.  I see you don\'t care. Moving on.' );
+    }
+}
+//if answering no to how many I can play. Still question 3.
+else if ( more.toLowerCase() === 'no' || more.toLowerCase() === 'n' ) {
+    console.log('Do I play other instruments? Answer:',  more );
+    const evenMore = prompt( 'Actually. I can play another instrument. You don\'t know me at all. How many do you think I can play?');
+    if ( evenMore === '2' || evenMore === '4' || evenMore === 'two' || evenMore === 'four') {
+        alert( 'Close. I play 3. Mandolin, Guitar and Bass.');
+        console.log( 'How many instruments can I play? Answer:', evenMore);
+    }
+    else if (evenMore === '3' || evenMore === 'three') {
+        confirm( 'Nailed it! I play 3. Mandolin, Guitar and Bass.');
+        console.log( 'How many instruments can I play? Answer:', evenMore);
+    }
+    else {
+        alert( 'It\s cool. I can tell you\'re bored. Moving on.' );
+        console.log( 'How many instruments can I play? Answer:', evenMore);
     }
 }
 
-var genre = prompt( 'Ok, moving on. Is my favorite genre acoustic instrumental?' );
-if (genre.toLowerCase() === 'yes' || genre.toLowerCase() === 'y' ) { 
+// Question four about my favorite genre
+const genre = prompt( 'Ok, moving on.' + username + ', is my favorite genre acoustic instrumental?' );
+if (genre.toLowerCase() === 'yes' || genre.toLowerCase() === 'y' ) {
     confirm( 'You damn skippy it is!');
-    console.log( 'Is my favorite genre acoustic instrumental?', genre);
+    console.log( 'Is my favorite genre acoustic instrumental? Answer:', genre);
 } else if ( genre.toLowerCase() === 'no' || genre.toLowerCase() === 'n' ) {
-    alert( 'I know it sounds weird but it is. You should check it out.' );
-    console.log( 'Is my favorite genre acoustic instrumental?', genre);
+    alert( 'I know it sounds weird but it is.' + username + '! You should check it out.' );
+    console.log( 'Is my favorite genre acoustic instrumental? Answer:', genre);
 };
 
-var oregon = prompt( 'Last question! I swear. Am I from Oregon?' );
-if (oregon.toLowerCase() === 'yes' || oregon.toLowerCase() === 'y' ) { 
-    confirm( 'Nope. I came from Seattle via California!');
-    console.log( 'Am I from Oregon?', oregon);
+// Last question asking if I am from Oregon.
+const oregon = prompt( 'Last question! I swear. Ok, ' + username + ', from what you know of me thus far do you think I am from Oregon?' );
+if (oregon.toLowerCase() === 'yes' || oregon.toLowerCase() === 'y' ) {
+    confirm( 'Nope. I came from Seattle via California! But not with a banjo on my knee and I don\'t know anyone named Susana.');
+    console.log( 'Am I from Oregon? Answer:', oregon);
 } else if ( oregon.toLowerCase() === 'no' || oregon.toLowerCase() === 'n' ) {
     alert( 'That\'s correct. I came from Seattle via California!' );
-    console.log( 'Am I from oregon?', oregon);
+    console.log( 'Am I from oregon? Answer:', oregon);
 };
 
 alert('You made it through the most boring "Get-to-know-me" ever! Here is a bit more about me and what I\'m doing now. You must be reeeeeally bored.');
